7fa1773bc27c09abd890a8bfb6f8d216fdf4ac24
Richard Hildred <rhildred@gmail.com>
2026-01-29 21:25:20 +0000

    working visual test for hello.js module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,15 @@
-import React, {useState} from 'react';
+import {Hello} from './Hello.jsx';
 import {
-  StyleSheet,
-  Text,
   View,
-  Pressable,
-  useWindowDimensions,
 } from 'react-native';
 
 export default function App() {
-  const {height} = useWindowDimensions();
-  const [number, setNumber] = useState(0);
-
-  function handlePress() {
-    setNumber(Math.round(Math.random() * 100));
-  }
-
   return (
-    <View style={[styles.container, {height}, StyleSheet.absoluteFill]}>
-      <Text>Random number: {number}</Text>
-      <View style={styles.br} />
-      <Pressable
-        style={({pressed}) => [
-          {
-            opacity: pressed ? 0.7 : 1,
-          },
-          styles.btn,
-        ]}
-        onPress={handlePress}>
-        <Text style={styles.btnText}>Generate a random number</Text>
-      </Pressable>
+    <View>
+      <Hello name="Rich" />
+      <Hello />
+      <Hello name="Biff" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  br: {
-    height: 12,
-  },
-  btn: {
-    backgroundColor: '#222',
-    padding: 10,
-  },
-  btnText: {
-    color: '#fff',
-  },
-});
